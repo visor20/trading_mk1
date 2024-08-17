bd86edfe24995819ce451a8dfc26d29a057df4dd
RichardViso <richardnviso@gmail.com>
2024-08-16 23:24:36 -0500

    initial commit

---
+++ readme.txt
@@ -0,0 +1,2 @@
+My first attempt at writing a backtest for a trading strategy.
+I may also venture into writing a system for execution (TBD).
---
+++ src/main.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import yfinance as yf
+
+def main():
+    print("hello trading world")
+
+    # import work ??
+    amzn = yf.download('AMZN', '2001-01-01', '2020-01-01')
+
+if __name__ == "__main__":
+    main()
+
