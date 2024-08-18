3ab578beb362f9af9253d62c2d5d923806e06da8
RichardViso <richardnviso@gmail.com>
2024-08-18 00:47:11 -0500

    load data for backtest

--- readme.txt
+++ readme.txt
@@ -1,2 +1,4 @@
 My first attempt at writing a backtest for a trading strategy.
 I may also venture into writing a system for execution (TBD).
+
+Using Yahoo Finance to gather data.
--- src/main.py
+++ src/main.py
@@ -1,13 +1,52 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 import yfinance as yf
+import pandas_market_calendars as mcal
+from datetime import datetime, date, timedelta
 
-def main():
-    print("hello trading world")
+def is_market_open(date, start_date, end_date):
+    nyse = mcal.get_calendar('NYSE')
+    open_days = nyse.valid_days(start_date, end_date)
+    return date in open_days.date
 
-    # import work ??
-    amzn = yf.download('AMZN', '2001-01-01', '2020-01-01')
+def load_backtest_data(
+    symbol=None,
+    test_start=None,
+    test_end=None,
+    granularity="1m"
+):
+    # establish start and end if undefined
+    if test_end == None:
+        test_end = date.today()
+    if test_start == None:
+        duration = timedelta(days=29)
+        test_start = test_end - duration
+     
+    # get yfinance ticker object to :wqextract data
+    ticker_obj = yf.Ticker(symbol)
+
+    # calculate first day so we do not start with empy dataframe
+    final_df = pd.DataFrame()
+    cur_day = test_start
+
+    # now increment until the end
+    while cur_day < test_end:
+        if is_market_open(cur_day, test_start, test_end):
+            temp_df = ticker_obj.history(interval=granularity, start=cur_day, end=(cur_day + timedelta(days=1)))
+            final_df = pd.concat([final_df, temp_df])
+        cur_day += timedelta(days=1)
+
+    final_df = final_df.sort_index()
+    return final_df
+
+
+def main(symbol):
+    data = load_backtest_data(symbol=symbol)
+    plt.figure()
+    plt.plot(data['Close'])
+    plt.show()
+
 
 if __name__ == "__main__":
-    main()
-
+    main('SPY')
