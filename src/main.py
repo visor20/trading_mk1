ee0f807e06fc74b08f31da3e32c7c0e27c7c6915
RichardViso <richardnviso@gmail.com>
2024-08-23 11:53:36 -0500

    debug update_backtest_data

--- src/main.py
+++ src/main.py
@@ -11,16 +11,36 @@ import os
 # constants
 MARKET = 'NYSE'
 SYM = 'SPY'
+OUTPUT_DIR = '..\\data'
 GRANULARITY = '1m'
 CREATE_BACKTEST_DATA_RANGE = timedelta(days=29)
 UPDATE_LOWER_BOUND = timedelta(0)
 UPDATE_UPPER_BOUND = timedelta(days=7)
 
 
+def get_file_path():
+    src_dir = os.path.dirname(os.path.abspath(__file__))
+    output_dir = os.path.join(src_dir, OUTPUT_DIR)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    file_name = SYM + '.csv'
+    return os.path.join(output_dir, file_name)
+
+
 def is_market_open(date, start_date, end_date):
     nyse = mcal.get_calendar(MARKET)
     open_days = nyse.valid_days(start_date, end_date)
     return date in open_days.date
+
+
+def remove_corrupted_day(df, day_of_month):
+    dates_to_drop = []
+    for date, row in df.iterrows():
+        if day_of_month in date:
+            dates_to_drop.append(date)
+    df.drop(dates_to_drop, inplace=True)
 
 
 def create_backtest_data(
@@ -35,7 +55,7 @@ def create_backtest_data(
     if test_start == None:
         duration = CREATE_BACKTEST_DATA_RANGE
         test_start = test_end - duration
-     
+
     # get yfinance ticker object to :wqextract data
     ticker_obj = yf.Ticker(symbol)
 
@@ -58,17 +78,17 @@ def update_backtest_data(
     file_path=None,
     granularity=GRANULARITY
 ):
-    final_df = pd.read_csv(file_path, index_col=0)
     ticker_obj = yf.Ticker(symbol)
+    final_df = pd.read_csv(file_path, index_col=0)
 
     last_date_str = final_df.index[-1]
-    last_date = datetime.strptime(last_date_str[:-len("-04:00")],"%Y-%m-%d %H:%M:%S").date()
-    now = date.today()
+    last_date = datetime.strptime(last_date_str[:-len("-04:00")],"%Y-%m-%d %H:%M:%S")
+    now = datetime.now()
 
     time_since_update = now - last_date
-    
+
     if UPDATE_LOWER_BOUND < time_since_update < UPDATE_UPPER_BOUND:
-        temp_df = ticker_obj.history(interval=granularity, start=last_date + timedelta(days=1), end=now + timedelta(days=1))
+        temp_df = ticker_obj.history(interval=granularity, start=last_date, end=now + timedelta(days=1))
         final_df = pd.concat([final_df, temp_df])
         print("result: update successful")
     else: 
@@ -78,24 +98,13 @@ def update_backtest_data(
 
 
 def main():
-
-    # get the current dir of main.py
-    src_dir = os.path.dirname(os.path.abspath(__file__))
-
-    # check if output directory exists
-    output_dir = os.path.join(src_dir, '..\\data')
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    file_name = SYM + '.csv'
-    file_path = os.path.join(output_dir, file_name)
-
     data = pd.DataFrame()
+    
+    file_path = get_file_path()
 
     # call to load data for the symbol
     if not os.path.exists(file_path):
         data = create_backtest_data(symbol=SYM)
-
     else:
         print("starting update ...")
         data = update_backtest_data(symbol=SYM, file_path=file_path)
@@ -106,21 +115,26 @@ def main():
     # ensure csv finishes writing
     try:
         data.to_csv(file_path)
-        print(f"file saved to {file_path}")
     except Exception as e:
         print(f"An error occured: {e}")
 
     # Iterate over stock time series data
     # This is where trading strategy where be called...
+    """
     minute_count = day_count = 0
     for date, row in data.iterrows():
-        change = row['Close'] - row['Open']
         
-        if '9:30' in str(date):
+        if '9:30:00' in str(date):
             day_count += 1
 
+            print("\n")
+            print(f"{date} has open")
+            
+        if '15:59:00' in str(date):
+            print(f"{date} has close")
+            
         minute_count += 1
-
+    """
 
 if __name__ == "__main__":
     main()
